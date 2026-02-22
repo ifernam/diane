e8f4b42178e3a2fb40b8bafc5cf94817524de2e1
Peter Freeman <peter_freeman_42@proton.me>
2026-02-22 19:25:34 +0300

    It is prohibited to change the '_slug' in 'Activity'. The following methods have been created in 'Activities': '__contains__', 'add_activity', 'add_connection', 'activity_by_slug' and '__len__'. 'from_yaml' is now a class method. Minor corrections and additions have been made to the docstrings.

--- src/activities.py
+++ src/activities.py
@@ -50,6 +50,12 @@ class Activity:
     def __post_init__(self) -> None:
         self.validate()
 
+    
+    def __setattr__(self, name, value):
+        if name == '_slug' and hasattr(self, '_slug'):
+            raise AttributeError("Slug is immutable.")
+        super().__setattr__(name, value)
+
 
     def __hash__(self) -> int:
         return hash(self._slug)
@@ -72,19 +78,22 @@ class Activity:
 
     @classmethod
     def from_dict(cls, slug: str, data: dict) -> Activity:
-        '''Constructs activity from the dictionary.'''
+        '''Constructs activity from the dictionary.
+        
+        Ignores parents.'''
 
         if not isinstance(data, dict):
             raise TypeError(f'Activity \'{slug}\' must be a mapping, got {type(data).__name__}.')
 
-        # Show a warning if there are extra fields in the activity entry.
+        # Show a warning if there are extra fields in the activity
+        # dictionary.
         allowed_keys = {'title', 'description', 'parents'}
         extra_keys = set(data) - allowed_keys
         if extra_keys:
             sorted_quoted_extra_keys = [f'\'{str(k)}\'' for k in sorted(extra_keys)]
             extra_keys_string = ', '.join(sorted_quoted_extra_keys)
             warnings.warn(
-                f'Activity entry with slug \'{slug}\' contains unknown fields: '
+                f'Activity dictionary corresponding to the slug \'{slug}\' contains unknown fields: '
                 f'{extra_keys_string}.',
                 stacklevel=2
             )
@@ -112,28 +121,117 @@ class Activity:
 
 @dataclass
 class Activities:
-    '''All activities.'''
+    '''Registry of activities'''
 
-    activities_graph: nx.DiGraph = field(default_factory=nx.DiGraph)
-    slug_to_activity: dict[str, Activity] = field(default_factory=dict)
+    _activities_graph: nx.DiGraph = field(default_factory=nx.DiGraph)
+    _slug_to_activity: dict[str, Activity] = field(default_factory=dict)
 
     
-    def validate(self) -> None:
+    def _validate(self) -> None:
 
-        if not nx.is_directed_acyclic_graph(self.activities_graph):
-            cycle = nx.find_cycle(self.activities_graph)
+        if not nx.is_directed_acyclic_graph(self._activities_graph):
+            cycle = nx.find_cycle(self._activities_graph)
             raise ValueError(f'Cycle detected: {cycle}.')
+        
+        
+    def __post_init__(self) -> None:
+        self._validate()
+        
     
+    def _resolve_activity(self, obj: Activity | str) -> Activity:
+        '''Checks for activity in the registry. If activity is found,
+        restores activity according to the slug.'''
+
+        if isinstance(obj, Activity):
+            slug = obj.slug
+        elif isinstance(obj, str):
+            slug = obj
+        else:
+            raise TypeError(f'\'obj\' must be \'Activity\' or \'str\'.')
+
+        try:
+            return self._slug_to_activity[slug]
+        except KeyError as e:
+            raise KeyError(f'Unknown activity: \'{slug}\'.') from e
+
+    
+    def __contains__(self, item: object) -> bool:
+        '''Checks whether the activity is contained in the registry.
+        
+        Only takes the slug into account.'''
+
+        if isinstance(item, Activity):
+            return item.slug in self._slug_to_activity
+        
+        if isinstance(item, str):
+            return item in self._slug_to_activity
+        
+        return False
+    
+
+    def add_activity(self, activity: Activity) -> None:
+        '''Adds activity to the registry.'''
+
+        if activity.slug in self._slug_to_activity:
+            raise ValueError(f'Activity {activity.slug} already exists.')
+
+        self._activities_graph.add_node(activity)
+        self._slug_to_activity[activity.slug] = activity
+
+
+    def add_connection(self, parent: Activity | str, child: Activity | str) -> None:
+        '''Adds a connection between activities in the registry.
+
+        Securely adds a parent -> child connection with validation.
+        It is assumed that the activities themselves are already
+        contained in the registry.'''
+
+        parent = self._resolve_activity(parent)
+        child = self._resolve_activity(child)
+
+        if parent is child:
+            raise ValueError('Self-loop is not allowed.')
+
+        if self._activities_graph.has_edge(parent, child):
+            return    # Connection already exists
+
+        if nx.has_path(self._activities_graph, child, parent):
+            raise ValueError(
+                f'Adding connection \'{parent.slug}\' -> \'{child.slug}\' creates a cycle.'
+            )
+
+        self._activities_graph.add_edge(parent, child)
+
+
+    def activity_by_slug(self, slug: str) -> Activity:
+        '''Restores activity by slug.
+        
+        Raises:
+            KeyError: if the activity with the specified slug
+                is not found in the registry.'''
+
+        try:
+            return self._slug_to_activity[slug]
+        except KeyError as e:
+            raise KeyError(f'Unknown activity: \'{slug}\'.') from e
+
 
     def clear(self) -> None:
         '''Clears all activities data.'''
 
-        self.activities_graph.clear()
-        self.slug_to_activity.clear()
+        self._activities_graph.clear()
+        self._slug_to_activity.clear()
+
+
+    def __len__(self) -> int:
+        '''Size of activities registry.'''
+
+        return len(self._slug_to_activity)
         
 
-    def load_from_yaml(self, filename: str) -> None:
-        '''Load activities from YAML file.'''
+    @classmethod
+    def from_yaml(cls, filename: str) -> Activities:
+        '''Constructs activities registry from YAML file.'''
 
         try:
             path = Path(filename)
@@ -147,50 +245,42 @@ class Activities:
             activities_data = data.get('activities')
             if not isinstance(activities_data, dict):
                 raise ValueError('\'activities\' must be a mapping.')
+            
+            activities = cls()
 
-            # Clear old data.
-            self.clear()
-
-            # Create activities.
+            # Load activities.
             for slug, item in activities_data.items():
                 if not isinstance(item, dict):
-                    raise ValueError('Each activity must be a mapping.')
+                    raise ValueError('Each activity entry must be a mapping.')
 
                 activity = Activity.from_dict(slug, item)
+                activities.add_activity(activity)
 
-                if activity.slug in self.slug_to_activity:
-                    raise ValueError(f'Duplicate slug: {activity.slug}.')
-
-                self.slug_to_activity[activity.slug] = activity
-                self.activities_graph.add_node(activity)
-
-            # Create connections.
+            # Load connections.
             for slug, item in activities_data.items():
-                parents = item.get('parents', [])
+                parents = item.get('parents')
 
                 if parents is None:
                     parents = []
-
+                
                 if not isinstance(parents, list):
                     raise ValueError(
                         f'\'parents\' of \'{slug}\' must be a list.'
                     )
 
-                child = self.slug_to_activity[slug]
+                child = activities.activity_by_slug(slug)
 
                 for parent_slug in parents:
-                    if parent_slug not in self.slug_to_activity:
-                        raise ValueError(
-                            f'Unknown parent \'{parent_slug}\' for activity \'{slug}\'.'
-                        )
+                    if not isinstance(parent_slug, str):
+                        raise ValueError(f'The parent slug \'{parent_slug}\' must be a string.')
 
-                    parent = self.slug_to_activity[parent_slug]
-                    self.activities_graph.add_edge(parent, child)
+                    parent = activities.activity_by_slug(parent_slug)
+                    activities.add_connection(parent, child)
 
-            self.validate()
+            return activities
         
         except FileNotFoundError as e:
-            raise ValueError(f'File not found: {filename}.') from e
+            raise FileNotFoundError(f'File not found: {filename}.') from e
         
         except yaml.YAMLError as e:
-            raise ValueError(f'Invalid YAML: {e}.') from e
+            raise yaml.YAMLError(f'Invalid YAML: {e}.') from e

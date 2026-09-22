e5f00893b017b9d54d3b1ec69a97032095bb260a
ifernam <peter_freeman_42@proton.me>
2026-09-22 20:00:35 +0300

    Extend `MarkdownEntriesRegisterConfig`
    Add `daily_note_name_format` and `daily_note_template_path`
    to `MarkdownEntriesRegisterConfig` and validate the latter.

--- src/diane/storage/markdown_entries_register.py
+++ src/diane/storage/markdown_entries_register.py
@@ -2,9 +2,28 @@ from collections.abc import Iterator
 from pathlib import Path
 from typing import Literal, override
 
+from pydantic import field_validator
+
 from diane.chrono import Timestamp
 from diane.entry import Entry
 from diane.storage import EntriesRegister, EntriesRegisterConfig
+
+from .entries_register import EntriesRegisterError, NotRelativePathError
+
+
+class MarkdownEntriesRegisterError(EntriesRegisterError):
+    """A general Markdown entries register error."""
+    ...
+
+
+class DailyNoteTemplateNotRelativePathError(
+    MarkdownEntriesRegisterError, NotRelativePathError
+):
+    """A path to a daily note's template is not relative.
+
+    For Pydantic's validation.
+    """
+    ...
 
 
 class MarkdownEntriesRegisterConfig(EntriesRegisterConfig):
@@ -13,10 +32,38 @@ class MarkdownEntriesRegisterConfig(EntriesRegisterConfig):
     Attributes:
         backend (Literal['markdown']): A markdown backend literal.
         path (Path): A daily notes subdirectory.
+        daily_note_name_format (str): A daily note's name `strftime`
+            format. For example, '%Y-%m-%d'.
+        daily_note_template_path (Path | None): An optional relative
+            path to a daily note's template in a repository.
+            For example, 'templates/daily_note_template'.
     """
 
     backend: Literal['markdown'] = 'markdown'
     path: Path = Path('daily_notes')
+    daily_note_name_format: str = '%Y-%m-%d'
+    daily_note_template_path: Path | None = None
+
+    @field_validator('daily_note_template_path')
+    @classmethod
+    def _check_daily_note_template_path(cls, v: Path | None) -> Path | None:
+        """Validate a daily note template path.
+
+        Args:
+            v (Path | None): A path to a daily note template.
+
+        Returns:
+            Path | None: The validated path.
+
+        Raises:
+            DailyNoteTemplateNotRelativePathError: If a path to a daily
+                note template is not relative.
+        """
+        if v is not None and v.is_absolute():
+            raise DailyNoteTemplateNotRelativePathError(
+                f"'daily_note_template_path' must be relative, got '{v}'."
+            )
+        return v
 
 
 class MarkdownEntriesRegister(EntriesRegister[MarkdownEntriesRegisterConfig]):
